f18176479e0a9e55420a5cf0a927ca46a93c034d
alexpros <8882977@mail.ru>
2017-10-31 16:48:20 +0300

    + temp blog + integrated problems

--- studombudsman/views.py
+++ studombudsman/views.py
@@ -7,7 +7,13 @@ from django.views import generic
 from django.utils import timezone
 from django.conf.urls import url
 
+from blog.models import News
+from problems.models import Problem
+
 
 
 def index(request):
-    return render(request, 'studombudsman/index.html')
+    problems_list = Problem.objects.order_by('name')
+    latest_news_list = News.objects.order_by('-pub_date')[:3]
+    context = {'problems_list': problems_list, 'news_0': latest_news_list[0], 'news_1': latest_news_list[1], 'news_2': latest_news_list[2]}
+    return render(request, 'studombudsman/index.html', context)
